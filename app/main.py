77d594435e4623300ec63422b2ffe911b275bef9
Jeuxu <iliacdmn@gmail.com>
2025-10-07 23:03:45 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+from os import path
+
+
+def copy_file(command: str) -> None:
+    if len(command) == 0:
+        return
+
+    command_parts = command.split(" ")
+    if (command_parts[0] != "cp"
+            or len(command_parts) < 3
+            or command_parts[1] == command_parts[2]
+            or not path.exists(command_parts[1])):
+        return
+
+    with (open(command_parts[1], "r") as file_in,
+          open(command_parts[2], "w") as file_out):
+        file_out.write(file_in.read())
